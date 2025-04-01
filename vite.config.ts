f5d9712b5111cc0b7795a3aa413d950a012668c2
AleTartaglia <alej.tartaglia@gmail.com>
2025-04-01 17:50:10 -0300

    fix images

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/finance-tracker/", // Ruta para GitHub Pages
+  base: "/hospitalia-finance-tracker/", // Ruta para GitHub Pages
 });
