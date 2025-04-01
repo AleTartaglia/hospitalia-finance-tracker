7d0017a1957a1f2ead03d2e1bdfe47d26e641b12
AleTartaglia <alej.tartaglia@gmail.com>
2025-04-01 18:13:51 -0300

    updated main

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,7 @@ import "./index.css";
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register(
-        "https://aletartaglia.github.io/hospitalia-finance-tracker/service-worker.js"
-      )
+      .register("service-worker.js")
       .then((reg) => console.log("Service Worker registered:", reg))
       .catch((err) =>
         console.error("Service Worker registration failed:", err)
